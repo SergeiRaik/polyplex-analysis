2ee01ee4c8329c1a406d8b2c87a0da527beaa8e4
SergeiRaik <raiksv@gmail.com>
2024-06-17 15:59:18 +0300

    msd files cleaning

---
+++ spt/MSD_to_csv.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+
+def convert_excel_to_csv(folder_path):
+    # Iterate over all files in the provided folder
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".xlsx") or filename.endswith(".xls"):
+            # Construct the full file path
+            file_path = os.path.join(folder_path, filename)
+            
+            # Read the Excel file
+            df = pd.read_excel(file_path, usecols="A:C", skiprows=10, nrows=301)
+            
+            # Save to CSV with the same name
+            csv_filename = os.path.splitext(filename)[0] + '.csv'
+            csv_path = os.path.join(folder_path, csv_filename)
+            df.to_csv(csv_path, index=False)
+            print(f"Converted {filename} to {csv_filename}")
+
+folder_path = '22 March 2p5/MSD'
+convert_excel_to_csv(folder_path)
